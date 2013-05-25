cafcdfed26b01b434d9f74e766be2bd5f73b3590
LE HOANG VINH <hvle1010@gmail.com>
2013-05-25 11:25:34 +0200

    jj

--- my-opsworks/recipes/cacti_config.rb
+++ my-opsworks/recipes/cacti_config.rb
@@ -7,4 +7,5 @@ end
 node[:opsworks][:layers]["db-master"][:instances].each do |instance, names|
  execute "add_node_cacti" do
 	command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
+ end
 end
