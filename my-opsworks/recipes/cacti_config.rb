4dda386c334f6b718457895355b22ae7c5ff3fcb
LE HOANG VINH <hvle1010@gmail.com>
2013-05-25 11:18:49 +0200

    cacti configure

--- my-opsworks/recipes/cacti_config.rb
+++ my-opsworks/recipes/cacti_config.rb
@@ -1,7 +1,10 @@
 node[:opsworks][:layers]["php-app"][:instances].each do |instance, names|
-
+ execute "add_node_cacti" do
+	command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
+ end
 end
 
-node[:opsworks][:layers]["php-app"][:instances].each do |instance, names|
-
+node[:opsworks][:layers]["db-master"][:instances].each do |instance, names|
+ execute "add_node_cacti" do
+	command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
 end
