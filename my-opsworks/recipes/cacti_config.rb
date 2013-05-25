0eccbc81a52feede4aa94d4695cd7ccf6888f9b1
LE HOANG VINH <hvle1010@gmail.com>
2013-05-25 14:50:42 +0200

    dd

--- my-opsworks/recipes/cacti_config.rb
+++ my-opsworks/recipes/cacti_config.rb
@@ -1,18 +1,21 @@
-node[:opsworks][:layers]["php-app"][:instances].each do |instance, names|
- execute "add_node_cacti" do
-	command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
- end
+execute "init_inventaire_instances" do
+   command ">/tmp/inventaire_instances"
 end
 
-node[:opsworks][:layers]["db-master"][:instances].each do |instance, names|
- execute "add_node_cacti" do
-	command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
+my-layers = ['php-app', 'db-master', 'admin'] 
+
+my-layers.each do |layer|
+
+ node[:opsworks][:layers]["#{layer}"][:instances].each do |instance, names|
+
+  execute "add_node_cacti" do
+	 command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
+  end
+ 
+  execute "create_inventaire_instances" do
+	 command "echo #{instance} >> /tmp/inventaire_instances"
+  end
  end
+ 
 end
 
-
-node[:opsworks][:layers]["admin"][:instances].each do |instance, names|
- execute "add_node_cacti" do
-	command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
- end
-end
