f5f13a6e5c1026b0db878b85a5a4a4b48dd5a441
LE HOANG VINH <hvle1010@gmail.com>
2013-05-25 16:03:37 +0200

    cc

--- my-opsworks/recipes/cacti_config.rb
+++ my-opsworks/recipes/cacti_config.rb
@@ -7,7 +7,7 @@ end
  node[:opsworks][:layers]["#{layer}"][:instances].each do |instance, names|
 
   execute "add_node_cacti" do
-	 command "/home/ec2-user/scripts/add_node_cacti.sh #{instance}"
+	 command "/home/ec2-user/scripts/add_node_cacti.sh #{instance} 2>/dev/null"
   end
  
   execute "create_inventaire_instances" do
@@ -22,11 +22,13 @@ bash "clean_obsolete_instance" do
   user "root"
   code <<-EOH
   LST_SVR=$(/usr/bin/php /var/lib/cacti/cli/remove_device.php --list-devices 2>/dev/null | awk '{ print $1 }' | tail -n+2)
+  echo "List servers in cacti => $LST_SVR"
 	for i in $LST_SVR
 	do
 		EX=$(grep $i /tmp/inventaire_instances_cacti)
-		if [[ -n $EX ]]
-		then
+		if [[ ! -n $EX ]]
+		then 
+		     echo "Remove $i"
 		     NODE_ID=`/usr/bin/php /var/lib/cacti/cli/remove_device.php  --list-devices | grep $i | awk '{ print $1 }'`
 			/usr/bin/php /var/lib/cacti/cli/remove_device.php  --device-id=$NODE_ID			
 		fi
