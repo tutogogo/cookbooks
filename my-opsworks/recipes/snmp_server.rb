c07d8ada026bc6bf008bc36bf6e88517e7450307
LE HOANG VINH <hvle1010@gmail.com>
2013-05-25 10:49:23 +0200

    dd

--- my-opsworks/recipes/snmp_server.rb
+++ my-opsworks/recipes/snmp_server.rb
@@ -12,31 +12,6 @@ template "/etc/snmp/snmpd.conf" do
   )
 end
 
-
 service "snmpd" do
   action [ :enable, :start ]
-end
-
-directory "/home/ec2-user/scripts" do
-  owner "ec2-user"
-  group "ec2-user"
-  mode "0755"
-  action :create
-  not_if { ::File.directory?("/home/ec2-user/scripts") }
-end
-
-cookbook_file "/home/ec2-user/scripts/add_node_cacti.sh" do
-  source "add_node_cacti.sh"
-  owner "ec2-user"
-  group "ec2-user"
-  mode "0755"
-  action :create
-end
-
-cookbook_file "/var/lib/cacti/cli/remove_device.php" do
-  source "remove_device.php"
-  owner "cacti"
-  group "cacti"
-  mode "0755"
-  action :create
 end
